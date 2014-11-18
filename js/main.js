d6f5d425729d2422e7cdd8b1e74f260788af918a
Jonathan Garvey <jon.d.garvey@gmail.com>
2014-11-18 11:46:56 -0700

    Add ng-message and ng-model-options + port to bootstrap

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-angular.module('Register', ['ngMaterial'])
-.controller('RegisterController', function(AttendeeService, $mdDialog) {
+angular.module('Register', ['ngMessages'])
+.controller('RegisterController', function(AttendeeService) {
     var register = this;
 
     // Attendees
     register.newAttendee = {};
-    register.newAttendee.subscribe = false;
+    register.newAttendee.subscribed = false;
 
     var getAttendees = function() {
       register.attendees = AttendeeService.all()
@@ -12,39 +12,33 @@ angular.module('Register', ['ngMaterial'])
 
     register.addAttendee = function(attendee) {
       AttendeeService.add(attendee);
+      clearForm();
     };
     register.removeAttendee = function(id) {
       AttendeeService.delete(id);
     };
 
-    getAttendees();
-
-    register.showConfirm = function(ev, data, action, id) {
-      var templates = {
-        attendee: {
-          copy: $mdDialog.confirm().title('Are you sure you want to delete attendee ' + data.firstName + ' ' + data.lastName + '?').ariaLabel('Delete Attendee??').ok('Yes').cancel('No').targetEvent(ev),
-          action: function() {register.removeAttendee(id)}
-        }
-      };
-
-      $mdDialog.show(templates[action].copy).then(function() {templates[action].action(id)}, function() {return false;});
+    var clearForm = function() {
+      register.newAttendee = {};
     };
+
+    getAttendees();
 })
 .service('AttendeeService', function() {
     var attendees = [
-      {firstName: 'John', lastName:'Doe'},
-      {firstName: 'James', lastName:'Monroe'},
-      {firstName: 'Oliver', lastName:'Twist'},
-      {firstName: 'Sally', lastName:'McDuffy'},
-      {firstName: 'Gollum', lastName:'Weirdermeister'},
-      {firstName: 'Wild', lastName:'Style'},
-      {firstName: 'Vetruvius', lastName:'the Great'},
-      {firstName: 'Lucy', lastName:'Scholtz'}
+      {firstName: 'John', lastName:'Doe', email:"john.doe@example.com", subscribed: true},
+      {firstName: 'James', lastName:'Monroe', email:"jmonroe@example.com", subscribed: false},
+      {firstName: 'Oliver', lastName:'Twist', email:"twisted@example.com", subscribed: true},
+      {firstName: 'Sally', lastName:'McDuffy', email:"duff-duff@example.com", subscribed: true},
+      {firstName: 'Gollum', lastName:'Weirdermeister', email:"precious@example.com", subscribed: false},
+      {firstName: 'Wild', lastName:'Style', email:"specialist@example.com", subscribed: true},
+      {firstName: 'Vetruvius', lastName:'the Great', email:"morgan@example.com", subscribed: false},
+      {firstName: 'Lucy', lastName:'Scholtz', email:"odyssey_times@example.com", subscribed: false}
     ];
 
     this.all = function() {return attendees;};
 
     this.add = function(attendee) {attendees.push(attendee);};
 
-    this.delete = function(id) {attendees.splice(id,1)};
+    this.delete = function(id) {attendees.splice(id,1);};
   });
